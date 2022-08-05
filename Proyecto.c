0398c1fce435ac90172cea21a68faf3f9760d460
Sebas211003 <110680964+Sebas211003@users.noreply.github.com>
2022-08-05 10:46:27 -0500

    Update Proyecto.c

--- Proyecto.c
+++ Proyecto.c
@@ -3,18 +3,17 @@
 main (){
 	int n,m;
 	
-	int i=0,j=0;
-	int m1 [18][15];	
+	int i=0,j=0;	
 	int m1 [19][15];	
 		
 		
-		for(i=0;i<7;i++){
+
 		for(i=0;i<8;i++){
 			for(j=0;j<=4;j++){
 				m1 [i][j]= rand()%2;
 			}
 		}
-		for(i=0;i<7;i++){
+		
 		for(i=0;i<8;i++){
 		printf("\n");
 			for(j=0;j<=4;j++){
@@ -22,15 +21,15 @@ main (){
 				
 			}
 		}	
-		for(i=0;i<7;i++){
+		
 		for(i=0;i<8;i++){
 		printf("\n");
-			for(j=4;j<7;j++){
+			for(j=5;j<7;j++){
 			for(j=5;j<7;j++){
 					m1 [i][j]= 9;		
             }
        }
-		for(i=0;i<7;i++){	
+			
 		for(i=0;i<8;i++){	
 		printf("\n");
 			for(j=5;j<7;j++){

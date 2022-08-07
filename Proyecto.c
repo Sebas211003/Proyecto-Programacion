e4916efbfe438815fecb8ce3e353024b06f3c2df
AndersonAaa <trastos.cv@gmail.com>
2022-08-06 20:30:13 -0500

    impression

--- Proyecto.c
+++ Proyecto.c
@@ -1,173 +1,87 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int tidyparking();
 
-main (){
+int op;
 
-	//Declaro la variable ParkingM1, se encargara de mostrar la disponibilidad de los lugares de parking
-	int parkingM1[16][19];
-	
+void tidyparking();
+void impresion ();
+
+//Declaro la variable ParkingM1, se encargara de mostrar la disponibilidad de los lugares de parking
+char parkingM1[16][19];
+
+int main (){
+
 	//Esta funcion ordenara la forma inicial del array, esta pensada para ejectutarse solo la primera vez
-	tidyparking(parkingM1); 
+	tidyparking(); 
 
 	//Inicio el programa
 	int flag = 0;
 	while(flag == 0){
-		printf(",------.   ,---.  ,------. ,--. ,--.,--.,--.  ,--. ,----.    "); 
-		printf("|  .--. ' /  O  | |  .--. '|  .'   /|  ||  ,'.|  |'  .-./    ");
-		printf("|  '--' ||  .-.  ||  '--'.'|  .   ' |  ||  |' '  ||  | .---. ");
-		printf("|  | --' |  | |  ||  ||  | |  ||   ||  ||  | `   |'  '--'  | ");
-		printf("`--'     `--' `--'`--' '--'`--' '--'`--'`--'  `--' `------'  ");
+		printf(",------.   ,---.  ,------. ,--. ,--.,--.,--.  ,--. ,----.    \n"); 
+		printf("|  .--. ' /  O  | |  .--. '|  .'   /|  ||  ,'.|  |'  .-./    \n");
+		printf("|  '--' ||  .-.  ||  '--'.'|  .   ' |  ||  |' '  ||  | .---. \n");
+		printf("|  | --' |  | |  ||  ||  | |  ||   ||  ||  | `   |'  '--'  | \n");
+		printf("`--'     `--' `--'`--' '--'`--' '--'`--'`--'  `--' `------'  \n");
 
-		printf("Bienvenido!");
-
-
-
+		printf("Bienvenido!\nEnseguida encontrara un mapa de la zona.\n\n");
+		impresion(); 
 		
 
+		scanf("%d", &op);
 	}
 
-	/*
-	int n,m;
-	
-	int i=0,j=0;	
-	int m1 [19][15];	
-		
-		
-
-		for(i=0;i<8;i++){
-			for(j=0;j<=4;j++){
-				m1 [i][j]= rand()%2;
-			}
-		}
-		
-		for(i=0;i<8;i++){
-		printf("\n");
-			for(j=0;j<=4;j++){
-				printf("%d",m1[i][j]);
-				
-			}
-		}	
-		
-		for(i=0;i<8;i++){
-		printf("\n");
-			for(j=5;j<7;j++){
-			for(j=5;j<7;j++){
-					m1 [i][j]= 9;		
-            }
-       }
-			
-		for(i=0;i<8;i++){	
-		printf("\n");
-			for(j=5;j<7;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=0;i<8;i++){
-		printf("\n");
-			for(j=7;j<=10;j++){
-					m1 [i][j]= rand()%2;		
-            }
-       }
-		for(i=0;i<8;i++){	
-		printf("\n");
-			for(j=7;j<=10;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=0;i<8;i++){
-		printf("\n");
-			for(j=11;j<=12;j++){
-					m1 [i][j]= 9;		
-            }
-       }
-		for(i=0;i<8;i++){	
-		printf("\n");
-			for(j=11;j<=12;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=0;i<8;i++){
-		printf("\n");
-			for(j=13;j<=15;j++){
-					m1 [i][j]= rand()%2;		
-            }
-       }
-		for(i=0;i<8;i++){	
-		printf("\n");
-			for(j=13;j<=15;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=8;i<=10;i++){
-		printf("\n");
-			for(j=0;j<=15;j++){
-					m1 [i][j]= 9;		
-            }
-       }
-		for(i=8;i<=10;i++){	
-		printf("\n");
-			for(j=0;j<=15;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=11;i<=18;i++){
-		printf("\n");
-			for(j=0;j<=4;j++){
-					m1 [i][j]= rand()%2;		
-            }
-       }
-		for(i=11;i<=18;i++){	
-		printf("\n");
-			for(j=0;j<=4;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=11;i<=18;i++){
-		printf("\n");
-			for(j=5;j<=6;j++){
-					m1 [i][j]= 9;		
-            }
-       }
-		for(i=11;i<=18;i++){	
-		printf("\n");
-			for(j=5;j<=6;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=11;i<=18;i++){
-		printf("\n");
-			for(j=7;j<=15;j++){
-					m1 [i][j]= rand()%2;		
-            }
-       }
-		for(i=11;i<=18;i++){	
-		printf("\n");
-			for(j=7;j<=15;j++){
-				printf("%d",m1[i][j]);
-			}
-		}
-		for(i=0;i<=18;i++){	
-		printf("\n");
-		printf("\t");
-			for(j=0;j<=15;j++){
-				printf("\t");
-				printf("%d",m1[i][j]);		
-	}
-}
-}
-*/
 } 
 
-int tidyparking(int tidyarray[16][19]){
+
+void tidyparking(){ //Esta funcion ordena el vector inicialmente, para que muestre todos los lugares disponibles. 
+
 	for( int i = 0; i < 16; i++ ) {
             for( int j = 0; j < 19; j++ ) {
-                if( i==0 || i==16-1 || j==0 || j==19-1 )
-                    tidyarray[i][j] = '';
+                    if( i==0 || i==16-1 || j==0 || j==19-1 )
+                    parkingM1[i][j] = '#';
                 else
-                    tidyarray[i][j] = 0;
+                    parkingM1[i][j] = ' ';
             }       
         }
-	return tidyarray; 
+	
+	for( int i = 1; i < 5; i++ ) {
+            for( int j = 1; j < 9; j++ ) {
+                    parkingM1[i][j] = '0';
+            }       
+        }
+	for( int i = 7; i < 10; i++ ) {
+            for( int j = 1; j < 9; j++ ) {
+                    parkingM1[i][j] = '0';
+            }       
+        }
+	for( int i = 12; i < 15; i++ ) {
+            for( int j = 1; j < 9; j++ ) {
+                    parkingM1[i][j] = '0';
+            }       
+        }
+
+	for( int i = 1; i < 4; i++ ) {
+            for( int j = 11; j < 18; j++ ) {
+                    parkingM1[i][j] = '0';
+            }       
+        }
+	
+	for( int i = 7; i < 15; i++ ) {
+            for( int j = 11; j < 18; j++ ) {
+                    parkingM1[i][j] = '0';
+            }       
+        }
+	
+}
+
+void impresion(){ //Esta funcion se encarga de imprimir el mapa 
+	
+	for (int i=0;i<16;i++){
+		for (int k=0;k<19;k++){
+			printf("%c  ",parkingM1[i][k]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
